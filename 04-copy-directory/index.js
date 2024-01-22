cb9d43f11a37d11b05d75c959ce8f762325f7f66
Tatsiana Fedaruk <tatyanafedoruk@icloud.com>
2024-01-22 10:58:35 +0100

    feat: copy directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,30 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const fromFolder = path.join(__dirname, 'files');
+const toFolder = path.join(__dirname, 'files-copy');
+
+function copyDir() {
+
+    fs.mkdir(toFolder, { recursive: true }, (error) => {
+        if (error) {
+            return console.error(error);
+        }
+
+        fs.readdir(fromFolder, { withFileTypes: true }, (error, files) => {
+            if (error) {
+                console.log(error.message)
+            } else {
+                files.forEach((file) => {
+                    const fromFile = path.join(fromFolder, file.name);
+                    const toFile = path.join(toFolder, file.name);
+                    fs.copyFile(fromFile, toFile, () => { });
+                });
+
+            }
+        });
+
+    });
+}
+
+copyDir();
